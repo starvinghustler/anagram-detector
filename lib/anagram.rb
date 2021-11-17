03ccb043bceb4172fe984c09c592e96796df5117
idobok <idobok12@gmail.com>
2021-11-17 15:02:35 +0200

    commit tree 7f2cd7d59c17f51f4b258c90844166ff5a6fe7ff

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(words)
+        @word = words
+    end
+    
+    def match(array)
+        array.select {|w| w.split("").sort == @word.split("").sort}
+    end
+                        #|w| w.split splits the whole syntax into just words in array
+                        #.sort == @word detects no matches
+
+
+
+                    end
