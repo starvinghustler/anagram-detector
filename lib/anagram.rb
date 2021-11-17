24301f024d65db0db5d00d75d34695fa17180b76
idobok <idobok12@gmail.com>
2021-11-17 15:05:04 +0200

    commit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(words)
+        @word = words
+    end
+    
+    def match(array)
+        array.select {|w| w.split("").sort == @word.split("").sort}
+    end
+                        #|w| w.split splits the whole syntax into just words in array
+                        #.sort == @word detects no matches
+                        #@word.split("").sort detects all the matches of said anagrams
+                    end
